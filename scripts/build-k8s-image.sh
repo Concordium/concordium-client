ede2f56cd3ab9b6b445ef7c40d332ac46d3e5afd
Martin Karlsen Jensen <mkj@concordium.com>
2019-08-27 15:37:06 +0200

    Fixed negated check

--- scripts/build-k8s-image.sh
+++ scripts/build-k8s-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z "$JENKINS_HOME" ]; then
+if [ ! -z "$JENKINS_HOME" ]; then
   git clone git@gitlab.com:Concordium/p2p-client.git
   cat p2p-client/src/proto/concordium_p2p_rpc.proto > concordium.proto
 else
