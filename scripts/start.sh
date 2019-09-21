f865dc859323634ed43487bc52cf1c03dd694a2a
Ian Johannesen <ij@concordium.com>
2019-09-21 10:30:36 +0200

    Introduce a way to sleep before starting generator so network can settle

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,6 +27,12 @@ then
     tar xzf /$NUM_BAKERS-bakers.tar.gz -C /tmp
     mv /tmp/genesis_data/baker-$ID-account.json /tx_generator_account.json
     rm -rf /tmp/genesis_data
+
+    if [ -n "$ARTIFICIAL_START_DELAY" ]; then
+      echo "Sleeping for $ARTIFICIAL_START_DELAY second(s) before starting generator so network can settle"
+      sleep $ARTIFICIAL_START_DELAY
+    fi
+
     /tx-generator --grpc-ip $GRPC_IP --grpc-port $GRPC_PORT --keyPair /tx_generator_account.json $ARGS
   else
     echo "Not running generator instance"
