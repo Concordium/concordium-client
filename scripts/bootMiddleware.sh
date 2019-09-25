0aae6ed71c2ef95d5a24dd6eb2fe9e962bedd4ea
Mario Rogic <mr@concordium.com>
2019-09-25 17:01:36 +0100

    Docker helper scripts for booting middleware by autodetecting a local running backer, and log packing for debugging

---
+++ scripts/bootMiddleware.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+normal()  { tput sgr0;    }
+red()     { tput setaf 1; }
+green()   { tput setaf 2; }
+yellow()  { tput setaf 3; }
+blue()    { tput setaf 4; }
+magenta() { tput setaf 5; }
+cyan()    { tput setaf 6; }
+white()   { tput setaf 7; }
+
+
+NODES=$(docker ps --filter "publish=10000" --format "{{.Ports}}" | cut -d' ' -f5 | cut -d':' -f2 | cut -d'-' -f1);
+
+arr=($NODES)
+
+NODE=${arr[0]}
+
+if [[ ! $NODE ]]; then
+  echo "$(red)No nodes found, are you sure the docker testnet is running?$(normal)"
+  exit
+else
+  echo "Booting with node at port: $(green)$NODE$(normal)"
+fi
+
+stack install simple-client:middleware
+
+NODE_URL="localhost:$NODE" ES_URL="http://localhost:9200" SIMPLEID_URL="http://localhost:8000" middleware
