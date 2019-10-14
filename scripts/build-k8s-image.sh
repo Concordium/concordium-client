c8459372828f0291e401cb0fabf4a6dfe2c67b8e
Ian Johannesen <ij@concordium.com>
2019-10-14 20:02:31 +0200

    Add simple-client and allow for overriding sha for p2p-client

--- scripts/build-k8s-image.sh
+++ scripts/build-k8s-image.sh
@@ -2,6 +2,10 @@
 
 if [ ! -z "$JENKINS_HOME" ]; then
   git clone git@gitlab.com:Concordium/p2p-client.git
+  if [ "$#" >= 1 ]
+  then
+    ( cd p2p-client && git checkout $1 )
+  fi
   cat p2p-client/src/proto/concordium_p2p_rpc.proto > concordium.proto
   cp p2p-client/scripts/genesis-data/*-bakers.tar.gz .
   VERSION=`( cd p2p-client && git rev-parse --verify HEAD )`
@@ -10,6 +14,7 @@ else
   cp ../p2p-client/scripts/genesis-data/*-bakers.tar.gz .
   VERSION=`( cd ../p2p-client && git rev-parse --verify HEAD )`
 fi
+
 
 echo "Going to build a transaction generator which matches p2p-client @ $VERSION"
 docker build -f scripts/Dockerfile -t 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/tx-generator:$VERSION .
