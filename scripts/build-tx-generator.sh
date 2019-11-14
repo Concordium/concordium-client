74c84d231897ee20aaf45109c74c53285937873f
Ian Johannesen <ij@concordium.com>
2019-11-14 10:55:03 +0100

    Align haskell build image

--- scripts/build-tx-generator.sh
+++ scripts/build-tx-generator.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
-
-apt-get update && apt-get install -y curl protobuf-compiler
-
-export PATH=$PATH:$HOME/.cargo/bin:$HOME/.local/bin
-
-curl -sSL https://get.haskellstack.org/ | sh
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-rustup default 1.39.0
+set -e
 
 mkdir -p -m 0600 ~/.ssh && ssh-keyscan gitlab.com >> ~/.ssh/known_hosts
-
 ./build-deps.sh
-
 stack --version
-
 ./stack build
