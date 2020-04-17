9c020164f33e56b91b88b8c46bd16c8354e12a10
Thomas Dinsdale-Young <ty@concordium.com>
2020-04-17 12:37:08 +0200

    Revise getSimpleTransactionStatus in line with CB-171.

--- src/Concordium/Client/TransactionStatus.hs
+++ src/Concordium/Client/TransactionStatus.hs
@@ -1,90 +1,78 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 module Concordium.Client.TransactionStatus where
 
 import Data.Aeson
-import Data.Aeson.TH
+import Data.Aeson.Types (parseEither, Pair)
 import Control.Monad.IO.Class
 import qualified Data.Text as Text
+import qualified Data.HashMap.Strict as HM
 
 import Concordium.Types.Execution
-import qualified Concordium.ID.Types as IDTypes
 import Concordium.Types
-import Concordium.Types.Utils
 
-import Concordium.Client.Types.TransactionStatus
 import Concordium.Client.GRPC
 
-data FailedTransferReason = InvalidTargetAccount
-                          | NonExistentAmount
-                          | MalformedTransaction
-                          | InsufficientEnergy
-                          deriving(Eq,Show)
-
-$(deriveJSON defaultOptions{constructorTagModifier=firstLower} ''FailedTransferReason)
-
-data SimpleTransactionResult = TransferSuccess { to :: !AccountAddress, amount :: !Amount }
-                             | NewAccount { accountAddress :: !AccountAddress }
-                             | NewCredential { credentialId :: !IDTypes.CredentialRegistrationID,
-                                               onAccount :: !AccountAddress
-                                             }
-                             | FailedTransfer {
-                                 reason :: !FailedTransferReason
-                                 }
-
-$(deriveJSON defaultOptions{sumEncoding=TaggedObject{
-                               tagFieldName = "outcome",
-                               contentsFieldName = "details"},
-                             constructorTagModifier = firstLower
-                           }
-   ''SimpleTransactionResult
-  )
-
-type SimpleTransactionSummary = TransactionSummary' SimpleTransactionResult
-
-type SimpleTransactionStatusResult = TransactionStatusResult' SimpleTransactionResult
-
--- |Get a simplified transaction status of a transaction which is
--- either a simple transfer from an account to an account, or
--- a credential deployment which either deploys a new credential onto
--- an existing account, or 
-getSimpleTransactionStatus :: MonadIO m => TransactionHash -> ClientMonad m (Either String SimpleTransactionStatusResult)
-getSimpleTransactionStatus hash = do
-  eitherStatus <- getTransactionStatus (Text.pack (show hash))
-  return $ do
-    status <- eitherStatus
-    case fromJSON status of
-      Error err -> Left err
-      Success TransactionStatusResult{..} -> do
-        newMap <- mapM processSummary tsrResults
-        return TransactionStatusResult{tsrResults = newMap,..}
-
-      where processSummary Nothing = return Nothing
-            processSummary (Just TransactionSummary{..}) = Just <$> 
-              case tsType of
-                Nothing -> -- credential deployment, only two possible outcomes
-                  case tsResult of
-                    TxSuccess [AccountCreated addr, _] ->
-                      return TransactionSummary{tsResult = NewAccount addr,..}
-                    TxSuccess [CredentialDeployed regId addr] ->
-                      return TransactionSummary{tsResult = NewCredential regId addr,..}
-                    es ->
-                      Left $ "Unexpected outcome of credential deployment: " ++ show es
-                Just TTTransfer -> -- simple transfer. This might be rejected.
-                  case tsResult of
-                    TxSuccess [Transferred{etTo = AddressAccount addr,..}] ->
-                      return TransactionSummary{tsResult = TransferSuccess{to = addr, amount = etAmount},..}
-                    TxReject (InvalidAccountReference _) ->
-                      return TransactionSummary{tsResult = FailedTransfer InvalidTargetAccount,..}
-                    TxReject (ReceiverAccountNoCredential _) ->
-                      return TransactionSummary{tsResult = FailedTransfer InvalidTargetAccount,..}
-                    TxReject (AmountTooLarge _ _) ->
-                      return TransactionSummary{tsResult = FailedTransfer NonExistentAmount,..}
-                    TxReject OutOfEnergy ->
-                      return TransactionSummary{tsResult = FailedTransfer InsufficientEnergy,..}
-                    TxReject SerializationFailure ->
-                      return TransactionSummary{tsResult = FailedTransfer MalformedTransaction,..}
-                    es ->
-                      Left $ "Unexpected outcome of simple transfer: " ++ show es
-                _ ->
-                  Left "Unsupported transaction type for simple statuses."
+getSimpleTransactionStatus :: MonadIO m => TransactionHash -> ClientMonad m (Either String Value)
+getSimpleTransactionStatus trHash = do
+    eitherStatus <- getTransactionStatus (Text.pack $ show trHash)
+    return $
+      eitherStatus >>= \case
+        Null -> return $ object ["status" .= String "absent"]
+        Object o -> do
+          parseEither (.: "status") o >>= \case
+            "received" -> return $ object ["status" .= String "received"]
+            "finalized" -> HM.toList <$> parseEither (.: "outcomes") o >>= \case
+              [(bh,outcome)] -> do
+                fields <- outcomeToPairs outcome
+                return $ object $ ["status" .= String "finalized", "blockHashes" .= [bh :: BlockHash]] <> fields
+              _ -> fail "expected exactly one outcome for a finalized transaction"
+            "committed" -> do
+              outcomes <- HM.toList <$> parseEither (.: "outcomes") o
+              fields <- outcomesToPairs (snd <$> outcomes)
+              return $ object $ ["status" .= String "committed", "blockHashes" .= (fst <$> outcomes :: [BlockHash])] <> fields
+            s -> fail ("unexpected \"status\": " <> s)
+        _ -> fail "expected null or object"
+      -- TransactionStatusResult{..} <- eitherStatus >>= parseEither parseJSON :: Either String TransactionStatusResult
+  where
+    outcomeToPairs :: TransactionSummary -> Either String [Pair]
+    outcomeToPairs TransactionSummary{..} = 
+      (["transactionHash" .= tsHash
+      , "sender" .= tsSender
+      , "cost" .= tsCost] <>) <$>
+      case tsType of
+        Nothing -> -- credential deployment
+          case tsResult of
+            TxSuccess [AccountCreated {}, _] ->
+              return ["outcome" .= String "newAccount"]
+            TxSuccess [CredentialDeployed {}] ->
+              return ["outcome" .= String "newCredential"]
+            es ->
+              Left $ "Unexpected outcome of credential deployment: " ++ show es
+        Just TTTransfer -> 
+          case tsResult of
+            TxSuccess [Transferred{etTo = AddressAccount addr,..}] ->
+              return ["outcome" .= String "transferSuccess", "to" .= addr, "amount" .= etAmount]
+            TxReject (InvalidAccountReference _) ->
+              return ["outcome" .= String "invalidTargetAccount"]
+            TxReject (ReceiverAccountNoCredential _) ->
+              return ["outcome" .= String "invalidTargetAccount"]
+            TxReject (AmountTooLarge _ _) ->
+              return ["outcome" .= String "nonExistentAmount"]
+            TxReject OutOfEnergy ->
+              return ["outcome" .= String "insufficientEnergy"]
+            TxReject SerializationFailure ->
+              return ["outcome" .= String "malformedTransaction"]
+            es ->
+              Left $ "Unexpected outcome of simple transfer: " ++ show es
+        _ ->
+          Left "Unsupported transaction type for simple statuses."
+    outcomesToPairs :: [TransactionSummary] -> Either String [Pair]
+    outcomesToPairs l = do
+      outcomes <- mapM outcomeToPairs l
+      case outcomes of
+        [] -> Left "Expected at least one transaction outcome for a committed transaction"
+        [o] -> return o
+        (h:r)
+          | all (h==) r -> return h
+          | otherwise -> return ["outcome" .= String "ambiguous"]
