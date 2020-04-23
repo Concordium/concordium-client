3339f7c885f3de89b209d9255a12a720d6635dd9
Ian Johannesen <ij@concordium.com>
2020-04-23 15:45:49 +0200

    Fixup docker container build for tx-generator

--- scripts/build-tx-generator.sh
+++ scripts/build-tx-generator.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+export STACK_ROOT="/build/.stack"
+
 mkdir -p -m 0600 ~/.ssh && ssh-keyscan gitlab.com >> ~/.ssh/known_hosts
 ./build-deps.sh
 stack --version
-./stack build
+./stack build --ghc-options -j4
+ls /build/.stack-work/dist/x86_64-linux/
