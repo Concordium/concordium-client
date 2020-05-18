ea5bf96b4a678ee86cd980838ca1209f0a8a49c4
Michael Bisgaard Olesen <mo@concordium.com>
2020-05-18 10:36:05 +0200

    Add baker ID field to GetNodeInfo

--- src/Concordium/Client/Runner.hs
+++ src/Concordium/Client/Runner.hs
@@ -1171,8 +1171,9 @@ printNodeInfo mni =
   case mni of
     Left err -> liftIO (putStrLn err)
     Right ni -> liftIO $ do
-      putStrLn $ "Node id: " ++ show (ni ^. CF.nodeId . CF.value)
+      putStrLn $ "Node ID: " ++ show (ni ^. CF.nodeId . CF.value)
       putStrLn $ "Current local time: " ++ show (ni ^. CF.currentLocaltime)
+      putStrLn $ "Baker ID: " ++ maybe "not a baker" show (ni ^. CF.maybe'consensusBakerId)
       putStrLn $ "Peer type: " ++ show (ni ^. CF.peerType)
       putStrLn $ "Baker running: " ++ show (ni ^. CF.consensusBakerRunning)
       putStrLn $ "Consensus running: " ++ show (ni ^. CF.consensusRunning)
