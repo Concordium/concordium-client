ce68413c2feae602d1a0ca7d9f44d9dd3d0fd8c6
Ian Johannesen <ij@concordium.com>
2020-06-15 14:59:00 +0200

    Adapt for new finalization benchmark system

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,9 +15,16 @@ then
       ARGS="$ARGS --log"
     fi
 
-    tar xzf /$NUM_BAKERS-bakers.tar.gz -C /tmp
-    mv /tmp/genesis_data/baker-$ID-account.json /tx_generator_account.json
-    rm -rf /tmp/genesis_data
+    if [ -n "$FINBENCH_NUM" ];
+    then
+      tar xzf /finbench-bakers.tar.gz -C /tmp
+      mv /tmp/genesis_data/baker-$ID-account.json /tx_generator_account.json
+      rm -rf /tmp/genesis_data
+    else
+      tar xzf /$NUM_BAKERS-bakers.tar.gz -C /tmp
+      mv /tmp/genesis_data/baker-$ID-account.json /tx_generator_account.json
+      rm -rf /tmp/genesis_data
+    fi
 
     if [ -n "$ARTIFICIAL_START_DELAY" ]; then
       echo "Sleeping for $ARTIFICIAL_START_DELAY second(s) before starting generator so network can settle"
