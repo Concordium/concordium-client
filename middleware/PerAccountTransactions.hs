c084aec996b420ec48b1161cb41c48ccee55bb78
Javier Sagredo <js@concordium.com>
2020-06-29 13:58:33 +0200

    Some comments and clarifications plus updating the submodule

--- middleware/PerAccountTransactions.hs
+++ middleware/PerAccountTransactions.hs
@@ -34,14 +34,22 @@ pageAccount accountAddr = getPage [EntryAccount ==. ByteStringSerialized account
 streamRawAccounts :: MonadIO m => AccountAddress -> AccountStream m
 streamRawAccounts accountAddr =
   let
+    -- entries will hold the stream of `Entry`s that correspond
+    -- to the requested address.
     entries =
       streamEntities [EntryAccount ==. ByteStringSerialized accountAddr]
                      EntryId
                      (PageSize 10)
                      Descend
                      (Range Nothing Nothing)
+    -- zipWithSummary gets the associated `Summary` for each `Entry` and zips it
+    -- together with the `Entry`. We never delete elements from the database and the
+    -- `entrySummary` field has a foreign key constraint and *MUST* exist on the
+    -- `Summaries` table and be unique, so we can use `Prelude.head` safely.
     zipWithSummary v@(Entity _ Entry{..}) = ((v,) . Prelude.head <$> selectList [SummaryId ==. entrySummary] [])
   in
+    -- we are chaining the stream of entries with the zipping with the associated summary
+    -- to generate in the end a `Conduit () (Entity Entry, Entity Summary) ...`
     entries .| Conduit.mapM zipWithSummary
 
 data PrettySummary =
@@ -68,10 +76,10 @@ makePretty :: (Entity Entry, Entity Summary) -> Either String PrettyEntry
 makePretty (Entity _ Entry{..}, Entity _ Summary{..}) = do
   let peAccount = unBSS entryAccount
       peBlockHash = unBSS summaryBlock
-      peBlockHeight = fromIntegral summaryHeight
+      peBlockHeight = summaryHeight
       peBlockTime = summaryTimestamp
-  peTransactionSummary <- case AE.fromJSON summarySummary of
-    AE.Error _ -> case AE.fromJSON summarySummary of
+  peTransactionSummary <- case AE.fromJSON summarySummary :: AE.Result TransactionSummary of
+    AE.Error _ -> case AE.fromJSON summarySummary :: AE.Result SpecialTransactionOutcome of
       AE.Error e ->  Left e
       AE.Success v -> Right $ SpecialTransaction v
     AE.Success v -> Right $ BlockTransaction v
@@ -81,7 +89,6 @@ streamAccounts :: (MonadIO m)
                   => AccountAddress -- ^Account address to query.
                   -> ConduitM () (Either String PrettyEntry) (ReaderT SqlBackend m) ()
 streamAccounts addr = streamRawAccounts addr .| Conduit.map makePretty
-
 
 -- |Stream transactions affecting a given account, starting from the most recent one.
 -- Starts a new database connection. For more flexiblity use 'streamAccounts' directly.
