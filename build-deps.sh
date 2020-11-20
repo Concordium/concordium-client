d6962de062f7df3d34334a1bfaf019991fa93ae9
Javier Sagredo <js@concordium.com>
2020-11-20 14:31:36 +0100

    Undo unrelated changes

---
+++ build-deps.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+RUST_SRC=./deps/crypto/rust-src
+
+if [ -d $RUST_SRC ]; then
+    cargo build --release --manifest-path $RUST_SRC/Cargo.toml
+    mkdir -p ./extra-libs
+    cp $RUST_SRC/target/release/*.so ./extra-libs
+fi
