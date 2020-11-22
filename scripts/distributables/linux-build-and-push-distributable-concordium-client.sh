3da1b94e904edc367a07ddb0ca184b27122e49a5
Aleš Bizjak <ab@concordium.com>
2020-11-22 11:31:16 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- scripts/distributables/linux-build-and-push-distributable-concordium-client.sh
+++ scripts/distributables/linux-build-and-push-distributable-concordium-client.sh
@@ -11,4 +11,8 @@ docker run -e GHC_VERSION \
        -v $(pwd)/out:/out \
        concordium/linux-distributable-concordium-client
 
+# The concordium-client binary is copied to the static-libraries bucket.
+# We could consider adding a `static-binaries` one instead, but the jenkins 
+# script likely should not copy the binary directly to the distribution bucket
+# since we have to be very careful about what version we publish.
 aws s3 cp out/concordium-client s3://static-libraries.concordium.com/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
