ed30b5cf5d9a0179cd775fb5bf3e4a2a4d0ed9bb
Adam B. Hansen <abh@concordium.com>
2020-12-09 16:34:20 +0100

    handle non ascii characters in account names in wallet imports without crashing on windows

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,22 @@ import           Concordium.Client.Runner
 import           Data.Maybe
 import           Options.Applicative
 import           System.Environment
-
+import           System.IO
 ccPrefs :: ParserPrefs
 ccPrefs = defaultPrefs { prefColumns = 110,
                          prefShowHelpOnEmpty = True}
 
+-- |makeSafe handles encoding errors when cli does not accept all unicode characters
+makeSafe :: Handle -> IO ()                         
+makeSafe h = do
+  ce' <- hGetEncoding h
+  case ce' of
+    Nothing -> return ()
+    Just ce -> mkTextEncoding ((takeWhile (/= '/') $ show ce) ++ "//TRANSLIT") >>=
+      hSetEncoding h
+
 main :: IO ()
 main = do
+  mapM_ makeSafe [stdout, stderr]
   showAllOpts <- lookupEnv "SHOW_ALL_OPTS"
   process =<< customExecParser ccPrefs (optsParser $ isJust showAllOpts)
