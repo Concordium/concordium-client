3dc299cb2eb64e9bd643e3740476e0bfa270b764
Kasper Dissing Bargsteen <kb@concordium.com>
2021-03-24 16:08:31 +0100

    Remove encryption test references

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,10 +4,8 @@ import Test.Hspec
 
 import qualified DataTests.DoubleWordSpec
 import qualified SimpleClientTests.CliSpec
-import qualified SimpleClientTests.EncryptionSpec
 
 main :: IO ()
 main = hspec $ parallel $ do
   SimpleClientTests.CliSpec.tests
-  SimpleClientTests.EncryptionSpec.tests
   DataTests.DoubleWordSpec.tests
