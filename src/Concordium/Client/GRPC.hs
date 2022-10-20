64dea6b4acd15c835dac695f1dae5d473c1df73c
Aleš Bizjak <ab@concordium.com>
2022-10-20 08:02:44 +0200

    Add support for using TLS encrypted connection.

--- src/Concordium/Client/GRPC.hs
+++ src/Concordium/Client/GRPC.hs
@@ -60,6 +60,8 @@ data GrpcConfig =
     , retryNum :: !Int
     -- |Timeout of each RPC call (defaults to 5min if not given).
     , timeout :: !(Maybe Int)
+    -- |Whether to use TLS or not.
+    , useTls :: !Bool
     }
 
 data EnvData =
@@ -119,7 +121,7 @@ mkGrpcClient config mLogger =
         case target config of
           Just t  -> [auth, ("target", BS8.pack t)]
           Nothing -> [auth]
-      cfg = (grpcClientConfigSimple (host config) (port config) False)
+      cfg = (grpcClientConfigSimple (host config) (port config) (useTls config))
                  { _grpcClientConfigCompression = uncompressed
                  , _grpcClientConfigHeaders = header
                  , _grpcClientConfigTimeout = Timeout (fromMaybe 300 (timeout config))
