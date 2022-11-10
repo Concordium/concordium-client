06cd5eca2a2c1b27f71a41cb0ba8019cc210f9a6
Viktor Fredslund-Hansen <vfh@concordium.com>
2022-11-10 17:29:37 +0100

    Added tests for parsing using schema with data from concordium-cargo.

--- test/SimpleClientTests/SchemaParsingSpec.hs
+++ test/SimpleClientTests/SchemaParsingSpec.hs
@@ -11,12 +11,16 @@ import Concordium.Client.Types.Contract.Schema
 
 import Test.Hspec
 import Test.HUnit
+import Control.Monad.List (forM, forM_, when)
+import Data.Serialize (runPut)
 
 
 schemaParsingSpec :: Spec
 schemaParsingSpec = describe "Smart contract schemas" $ do
   schemaV1Parsing
   schemaV2Parsing
+  schemaV3Parsing2
+  schemaV3Parsing3
 
 -- Test that parsing a parameter that contains a
 -- - ULEB128 value in the amount field.
@@ -51,3 +55,52 @@ schemaV2Parsing = specify "Parse v2 schema" $ do
         Just schema -> case putJSONUsingSchema schema value of
           Left err -> assertFailure $ "Could not convert JSON according to the provided schema: " ++ err
           Right _ -> return ()
+
+
+-- | Module schema V3 tests 
+prefix :: String
+prefix = "test/data/cis2_wCCD/"
+
+getSchema :: IO SchemaType
+getSchema = do
+  schemaBytes <- BS.readFile $ prefix <> "cis2_wCCD_schema.bin"
+  moduleSchema <- case decodeVersionedModuleSchema schemaBytes of
+      (Left err) -> assertFailure $ "Could not parse schema: " ++ err
+      (Right moduleSchema) -> return moduleSchema
+  case lookupEventSchema moduleSchema "cis2_wCCD" of
+    Just schema -> return schema
+    Nothing -> assertFailure "No schema for named contract 'cis2_wCCD'."
+
+-- | Deserializing and serializing a bytestring using a module schema containing TaggedEnum is the identity function.
+schemaV3Parsing2 :: Spec
+schemaV3Parsing2 = specify "Deserializing and serializing a bytestring using a module schema containing TaggedEnum is the identity function" $ do
+  schema <- getSchema
+  -- Assert that raw inputs can be deserialized to JSON according to schema.
+  json <- forM [0,1] $ \(i :: Integer) -> do
+    eventBytes <- BS.readFile $ prefix <> "event" <> show i <> ".bin"
+    case decodeParameter schema eventBytes of
+      Left err -> assertFailure $ "Unable to decode event " <> show i <> ": " <> err
+      Right val -> return (i, eventBytes, val)
+  -- Assert that deserialized values can be encoded to the 
+  forM_ json $ \(i, bs, val) -> do
+    case encodeParameter schema val of
+      Left err -> assertFailure $ "Unable to encode event " <> show i <> ": " <> err
+      Right bytes -> do
+        when (bytes /= bs) $ assertFailure ""
+
+-- | Serializing cargo-concordium JSON output using a module schema containing a TaggedEnum yields the corresponding expected raw output.
+schemaV3Parsing3 :: Spec
+schemaV3Parsing3 = specify "Serialize JSON using a module schema containing TaggedEnum" $ do
+  schema <- getSchema
+  -- Assert that we can parse and serialize all JSON files according to schema.
+  res <- forM [0,1] $ \(i :: Integer) -> do
+    json <- AE.decodeFileStrict $ prefix <> "event" <> show i <> ".json"
+    case json of
+      Just value -> case putJSONUsingSchema schema value of
+        Left err -> assertFailure $ "Could not convert JSON according to the provided schema: " ++ err
+        Right str -> return (i, str)
+      _ -> assertFailure "Input is not valid JSON."
+  -- Assert that serialized JSON files match expected binary output.
+  forM_ res $ \(i, put) -> do
+    eventBytes <- BS.readFile $ prefix <> "event" <> show i <> ".bin"
+    when (runPut put /= eventBytes) $ assertFailure $ "Serialized JSON does not match expected output for event " <> show i <> "."
---
+++ test/data/cis2_wccd/README.md
@@ -0,0 +1,5 @@
+cis2_wCCD testdata
+==================
+Contains testdata generated from cargo-concordium using the cis2_wCCD smart contract example:
+- event[1|2][.json|.raw] contains JSON representation, respectively raw event data.
+- cis2_wCCD_schema.bin contains the V3 schema produced by concordium-cargo.
